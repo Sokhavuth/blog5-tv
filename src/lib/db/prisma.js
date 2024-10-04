842e236e3a1e6e4baac780ae64098c4cb1b27775
khmerweb <vuthdevelop1@gmail.com>
2024-10-04 11:34:50 +0700

    Make it better

--- src/lib/db/prisma.js
+++ src/lib/db/prisma.js
@@ -3,27 +3,18 @@ import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { createClient } from '@libsql/client'
 import prismaRandom from 'prisma-extension-random'
 
+const libsql = createClient({
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  //url: `file:./db/dev.db`,
+  //syncUrl: `${process.env.TURSO_DATABASE_URL}`,
+})
+const adapter = new PrismaLibSQL(libsql)
 let prisma
 
 if (process.env.NODE_ENV === "production") {
-  const libsql = createClient({
-    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-    url: `${process.env.TURSO_DATABASE_URL}`,
-    //url: `file:./db/dev.db`,
-    //syncUrl: `${process.env.TURSO_DATABASE_URL}`,
-  })
-  const adapter = new PrismaLibSQL(libsql)
-
   prisma  = new PrismaClient({ adapter }).$extends(prismaRandom())
 } else {
-  const libsql = createClient({
-    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
-    //url: `${process.env.TURSO_DATABASE_URL}`,
-    url: `file:./client/db/dev.db`,
-    syncUrl: `${process.env.TURSO_DATABASE_URL}`,
-  })
-  const adapter = new PrismaLibSQL(libsql)
-
   if (!global.prisma) {
     global.prisma  = new PrismaClient({ adapter }).$extends(prismaRandom())
   }
